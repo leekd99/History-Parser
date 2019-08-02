4aa552e5b36f4a8c69cebd148ff15de22ab3cfc5
leekd99 <lee.kyle.d@gmail.com>
2019-08-02 13:07:36 -0400

    First commit

---
+++ HistoryParser/src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * History parser for getting the address of the particular pages visited for any site
+ * This will retrieve the users history for google chrome as a comma delimitted csv file
+ * Then it will take as input the address of the site's main page followed by the 
+ * column index at which the address of the particular page is stored in the csv file
+ * Since the index can be chosen it'll potentially be compatible with other broswers history
+ * given they can get their history in a csv file
+ * 
+ * Author: Kyle D. Lee
+ * Date: 08-02-2019
+ */
+
+
+
+public class Main {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Wazz up my homies!");
+		
+	}//end main
+	
+}//end main class
