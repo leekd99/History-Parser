12ec054a596b43930df93a80c6bdbe57b35e5f1c
leekd99 <lee.kyle.d@gmail.com>
2019-08-02 14:34:55 -0400

    Finished UI
    Finished adding all the necessary components to the GUI

--- HistoryParser/src/HP_GUI.java
+++ HistoryParser/src/HP_GUI.java
@@ -8,21 +8,17 @@ public class HP_GUI {
 	private JFrame HistoryParserFrame;			//main Frame to hold everything in
 	private String UI_title = "History Parser";	//Title of main frame
 	private int UI_height = 600;				//Height of the UI
-	private int UI_width = 500;					//Width of the UI
-	private int grid_row = 3;					//number of rows for UI
+	private int UI_width = 300;					//Width of the UI
+	private int grid_row = 2;					//number of rows for UI
 	private int grid_col = 1;					//number of columns for UI
 	
 	private String filename_label = "File Name";
 	private String sitename_label = "Site Name";
 	private String siteindex_label = "Site Index";
-	
+	private String execute_button_label = "Get Site List";
 	
 	private JPanel firstRow;					//Holds the first row which will hold the message box
 		
-		private JTextArea messageBoard;				//textArea to inform user of status and errors
-	
-	private JPanel secondRow;					//Holds the second row which will hold the inputs
-	
 		private JLabel filenamePrompt;			//label for where to put file name
 		private JLabel sitenamePrompt;			//label for where to put site name
 		private JLabel siteindexPrompt;			//label for where to put site index
@@ -31,15 +27,17 @@ public class HP_GUI {
 		private JTextField sitenameInput;			//text field to put sitename into
 		private JTextField siteindexInput;			//text field to put site index into
 	
-	private JPanel thirdRow;					//Holds the third row which will hold the buttons
-	
 		private JButton execute;					//Button to let program start
+
+	private JPanel secondRow;					//Holds the second row which will hold the inputs
+		
+		private JTextArea messageBoard;				//textArea to inform user of status and errors
 	
 	public HP_GUI() {
 		
 		//TODO add calls to initialize methods
 		initializeGUI();
-		initializeInput();
+		initialize();
 		
 		
 		HistoryParserFrame.setVisible(true);
@@ -63,7 +61,7 @@ public class HP_GUI {
 	/*
 	 * initialize function to setup inputs
 	 */
-	private void initializeInput() {
+	private void initialize() {
 		
 		//create new JPanel
 		firstRow = new JPanel();
@@ -71,6 +69,7 @@ public class HP_GUI {
 		//Create GridBagLayout for firstRow Panel and a GridBagCOnstraint object to set contraints for components
 		GridBagLayout firstRowLayout =  new GridBagLayout();
 		GridBagConstraints firstRowConstraints = new GridBagConstraints();
+		
 		
 		//set layout of JPanel to GridBagLayout
 		firstRow.setLayout(firstRowLayout);
@@ -90,10 +89,9 @@ public class HP_GUI {
 		siteindexInput = new JTextField();
 		siteindexInput.setPreferredSize(new Dimension(200,20));
 		
-		//private JTextField filenameInput;			
-		//private JTextField sitenameInput;			
-		//private JTextField siteindexInput;	
-		
+		//set button
+		execute = new JButton(execute_button_label);
+
 		//add filename prompt
 		firstRowConstraints.fill = GridBagConstraints.HORIZONTAL;
 		firstRowConstraints.gridx = 0;
@@ -136,8 +134,30 @@ public class HP_GUI {
 		firstRowConstraints.gridwidth = 2;
 		firstRow.add(siteindexInput, firstRowConstraints);
 		
+		//add button
+		firstRowConstraints.fill = GridBagConstraints.HORIZONTAL;
+		firstRowConstraints.gridx = 1;
+		firstRowConstraints.gridy = 3;
+		firstRowConstraints.gridwidth = 1;
+		firstRow.add(execute, firstRowConstraints);
+		
 		//add to frame
 		HistoryParserFrame.add(firstRow);
+		
+		//add second row
+		secondRow = new JPanel();
+		//Create GridBagLayout for firstRow Panel and a GridBagCOnstraint object to set contraints for components
+		GridBagLayout secondRowLayout =  new GridBagLayout();
+		GridBagConstraints secondRowConstraints = new GridBagConstraints();
+		
+		//set text Area
+		messageBoard = new JTextArea(10,25);
+		messageBoard.setWrapStyleWord(true);
+				
+		secondRowConstraints.fill = GridBagConstraints.CENTER;
+		secondRow.add(messageBoard,secondRowConstraints);
+				
+		HistoryParserFrame.add(secondRow);
 		
 	}//end initializeDisplay()
 	
